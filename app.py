94cc73e282916462ddfb35972b37ae0469995f73
Oum789 <february.sena771@gmail.com>
2025-02-26 13:19:29 +0700

    add code

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route('/getcode')
 def get_code():
     team = 'group5: นิ่งไว้'
     return team
+    
+@app.route('/cir_round/<x>')
+def cir_round(x):
+    x = float(x)
+    if x < 0:
+        return "0.00"
+    return str(2*3.14*x)
 
-@app.route('/pita/<int:a>/<int:b>/<int:c>')
-def pita(a,b,c):
-    return f'Yes' if (a*a+b*b) == (c*c) else f"No"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- tests/test_plus.py
+++ tests/test_plus.py
@@ -4,13 +4,18 @@ from app import app
 class TestAPI(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-    def test_plus1(self):
-        response = self.app.get('/pita/3/4/5')
-        self.assertEqual(response.data.decode('utf-8'), 'Yes')
 
-    def test_plus2(self):
-        response = self.app.get('/pita/3/4/6')
-        self.assertEqual(response.data.decode('utf-8'), 'No')
+    def test_x_is_1(self):
+        response = self.app.get('/cir_round/1')
+        self.assertEqual(response.data.decode('utf-8'), '6.28')
+
+    def test_x_is_neg10(self):
+        response = self.app.get('/cir_round/-10')
+        self.assertEqual(response.data.decode('utf-8'), '0.00')
+
+    def test_x_is_1dot5(self):
+        response = self.app.get('/cir_round/1.5')
+        self.assertEqual(response.data.decode('utf-8'), '9.42')
 
 if __name__ == '__main__':
     unittest.main()
